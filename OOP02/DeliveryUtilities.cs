0f4a8ae95a3515b6609485d498423b6b4b1c491b
noorhossamm <noormwafy78@gmail.com>
2026-08-19 14:31:18 +0300

    Assignment05( Part02) - Question07

---
+++ OOP02/DeliveryUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP02
+{
+    internal static class DeliveryUtilities
+    {
+
+        public static void PrintSeparator()
+        {
+            Console.WriteLine("----------------------------------");
+        }
+
+        public static void PrintSystemTitle()
+        {
+            PrintSeparator();
+            Console.WriteLine("Delivery Center");
+            PrintSeparator();
+        }
+    }
+}
